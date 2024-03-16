4228a9c35fae554605d02c359365b979313a95a5
Bartimacrez <sjm2124@live.co.uk>
2024-03-16 13:39:19 +0000

    Adding test commit with the test push commit comment in SpringbootThmyleafCrudWebApplication.java

--- src/main/java/prt/springbootthymeleafcrudwebapp/SpringbootThymeleafCrudWebAppApplication.java
+++ src/main/java/prt/springbootthymeleafcrudwebapp/SpringbootThymeleafCrudWebAppApplication.java
@@ -3,6 +3,7 @@ package prt.springbootthymeleafcrudwebapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Test push commit
 @SpringBootApplication
 public class SpringbootThymeleafCrudWebAppApplication {
 
